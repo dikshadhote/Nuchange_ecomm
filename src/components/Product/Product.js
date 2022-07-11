70f8c73ff45605a24a78d87ece5464a802b584e8
Diksha Dhote <dikshadhote7042@gmail.com>
2022-07-12 00:09:13 +0530

    feat: label in or out of stock on items availibility

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,28 +2,25 @@ import React from "react";
 
 export default function Product({ product }) {
   console.log(product);
-  const { name, category, image, price } = product;
+  const { name, category, image, price, available } = product;
   return (
     <div className="card flex-column card-vert">
+      <span class="badge square-badge persian-blue-bg p-1">
+        <span class="white-text-color">
+          {available ? "In Stock" : "Out Of Stock"}{" "}
+        </span>
+      </span>
+
       <img src={image} className="card-img-vert" />
       <div className="card-body">
-        <a className="card-title" href="#card">
-          {name}
-        </a>
+        <a className="card-title">{name}</a>
         <span className="card-subtitle">{category}</span>
         <p className="card-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit...
         </p>
         <div className="price-container">
-          <span className="discount-price">{price}</span>
+          <span className="discount-price">Rs{price}</span>
         </div>
-        <button
-          href="#"
-          className="btn persian-blue-bg white-text-color btn-card btn-size"
-        >
-          <span className="material-icons card-btn-icon">shopping_cart</span>
-          <p className="">Move to cart</p>
-        </button>
       </div>
     </div>
   );
